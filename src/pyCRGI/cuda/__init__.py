0764953a71df0a56d3e24809a29727044a4eb88e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-06-25 14:17:57 +0200

    hide device functions

--- src/pyCRGI/cuda/__init__.py
+++ src/pyCRGI/cuda/__init__.py
@@ -5,16 +5,16 @@ JIT-compiled implementation, 2nd experimental edition
 """
 
 from ._calculate import (
-    geodetic2geocentric,
+    # geodetic2geocentric,
     get_syn,
 )
 
-from ._coeffs import (
-    get_g_coeff,
-    get_h_coeff,
-    get_coeffs_prepare,
-    GH,
-)
+# from ._coeffs import (
+#     get_g_coeff,
+#     get_h_coeff,
+#     get_coeffs_prepare,
+#     GH,
+# )
 
 from ._value import (
     get_value,
