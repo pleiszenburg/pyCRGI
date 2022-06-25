74365768814f5e6a79ce62db2aad85fc6df175d7
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-06-25 14:49:30 +0200

    basic kernel and wrapper, alloc error

--- src/pyCRGI/cuda/_calculate.py
+++ src/pyCRGI/cuda/_calculate.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from math import sin, cos, sqrt, atan2, pi
-
-from ._coeffs import (
-    get_g_coeff,
-    get_h_coeff,
-    get_coeffs_prepare,
-)
+from typing import Optional, Union
 
 from numba import cuda
 import numpy as np
 
-
-FACT = 180.0 / pi
+from .._debug import typechecked
+from ._coeffs import (
+    get_g_coeff,
+    get_h_coeff,
+    get_coeffs_prepare,
+    GH,
+)
 
 
 @cuda.jit('Tuple([f8,f8,f8])(f8,f8)', device = True)
@@ -47,50 +47,49 @@ def geodetic2geocentric(theta, alt):
     return gccolat, d, r
 
 
-@cuda.jit('f8[:](f8,i8,f8,f8,f8)')  # TODO
-def _get_syn(year, itype, alt, lat, elong): # TODO check 12th gen vs 13th gen synthesis routine
+@cuda.jit('void(f8[:],i8,f8[:],f8[:],f8[:],f8[:],f8[:,:])')
+def _get_syn(
+    years,
+    itype,
+    alts,
+    lats,
+    elongs,
+    gh,
+    xyzfs,
+):
     """
-    This is a synthesis routine for the 12th generation IGRF as agreed
-    in December 2014 by IAGA Working Group V-MOD. It is valid 1900.0 to
-    2020.0 inclusive. Values for dates from 1945.0 to 2010.0 inclusive are
-    definitive, otherwise they are non-definitive.
-
-    To get the other geomagnetic elements (D, I, H and secular
-    variations dD, dH, dI and dF) use routines ptoc and ptocsv.
-
-    Adapted from 8th generation version to include new maximum degree for
-    main-field models for 2000.0 and onwards and use WGS84 spheroid instead
-    of International Astronomical Union 1966 spheroid as recommended by IAGA
-    in July 2003. Reference radius remains as 6371.2 km - it is NOT the mean
-    radius (= 6371.0 km) but 6371.2 km is what is used in determining the
-    coefficients. Adaptation by Susan Macmillan, August 2003 (for
-    9th generation), December 2004, December 2009, December 2014.
-
-    Coefficients at 1995.0 incorrectly rounded (rounded up instead of
-    to even) included as these are the coefficients published in Excel
-    spreadsheet July 2005.
+    CUDA kernel
 
     Args:
-        year : year A.D. Must be greater than or equal to 1900.0 and
+        years : year A.D. Must be greater than or equal to 1900.0 and
             less than or equal to 2025.0. Warning message is given
             for dates greater than 2020.0. Must be double precision.
         itype : 1 if geodetic (spheroid), 2 if geocentric (sphere)
-        alt : height in km above sea level if itype == 1,
+        alts : height in km above sea level if itype == 1,
             distance from centre of Earth in km if itype == 2 (>3485 km)
-        lat : latitude (-90 to 90)
-        elong : east-longitude (0 to 360)
-    Returns:
-        x, north component [nT] if isv == 0, [nT/year] if isv == 1;
-        y, east component [nT] if isv == 0, [nT/year] if isv == 1;
-        z, vertical component [nT] if isv == 0, [nT/year] if isv == 1;
-        f, total intensity [nT] if isv == 0, [rubbish] if isv == 1
+        lats : latitude (-90 to 90)
+        elongs : east-longitude (0 to 360)
+        gh : g and h
+        xyzfs : north, east, verical, total; [nT] if isv == 0, [nT/year] if isv == 1 (output)
     """
+
+    idx = cuda.grid(1)
+    if idx >= xyzfs.shape[0]:
+        return
+
+    year = years[idx]
+    alt = alts[idx]
+    lat = lats[idx]
+    elong = elongs[idx]
 
     x, y, z = 0., 0., 0.
 
     if year < 1900.0 or year > 2030.0:
-        f = 1.0
-        return np.array([x, y, z, f], dtype = 'f8')
+        xyzfs[idx, 0] = x
+        xyzfs[idx, 1] = y
+        xyzfs[idx, 2] = z
+        xyzfs[idx, 3] = 1.0  # f
+        return
 
     p = np.zeros((105,), dtype = 'f8')
     q = np.zeros((105,), dtype = 'f8')
@@ -103,11 +102,11 @@ def _get_syn(year, itype, alt, lat, elong): # TODO check 12th gen vs 13th gen sy
     colat = 90. - lat
     r = alt
 
-    one = colat / FACT
+    one = colat * pi / 180
     ct = cos(one)
     st = sin(one)
 
-    one = elong / FACT
+    one = elong * pi / 180
     cl[0] = cos(one)
     sl[0] = sin(one)
 
@@ -159,13 +158,13 @@ def _get_syn(year, itype, alt, lat, elong): # TODO check 12th gen vs 13th gen sy
                 cl[m - 1] = cl[m - 2] * cl[0] - sl[m - 2] * sl[0]
                 sl[m - 1] = sl[m - 2] * cl[0] + cl[m - 2] * sl[0]
         # synthesis of x, y and z in geocentric coordinates
-        one = get_g_coeff(n, m, ll, tt, tc, nc) * rr
+        one = get_g_coeff(n, m, ll, tt, tc, nc, gh) * rr
         if m == 0:
             x = x + one * q[k - 1]
             z = z - (fn + 1.0) * one * p[k - 1]
             l = l + 1
         else:
-            two = get_h_coeff(n, m, ll, tt, tc, nc) * rr
+            two = get_h_coeff(n, m, ll, tt, tc, nc, gh) * rr
             three = one * cl[m - 1] + two * sl[m - 1]
             x = x + three * q[k - 1]
             z = z - (fn + 1.0) * three * p[k-1]
@@ -182,4 +181,88 @@ def _get_syn(year, itype, alt, lat, elong): # TODO check 12th gen vs 13th gen sy
     z = z * cd - one * sd
     f = sqrt(x * x + y * y + z * z)
 
-    return np.array([x, y, z, f], dtype = 'f8')
+    xyzfs[idx, 0] = x
+    xyzfs[idx, 1] = y
+    xyzfs[idx, 2] = z
+    xyzfs[idx, 3] = f
+
+
+@typechecked
+def get_syn(
+    years: Union[float, np.ndarray], # f8 n
+    itype: int,
+    alts: np.ndarray, # f8 n
+    lats: np.ndarray, # f8 n
+    elongs: np.ndarray, # f8 n
+    xyzfs: Optional[np.ndarray] = None, # f8 n * 4
+) -> np.ndarray: # f8 n * 4
+    """
+    This is a synthesis routine for the 12th generation IGRF as agreed
+    in December 2014 by IAGA Working Group V-MOD. It is valid 1900.0 to
+    2020.0 inclusive. Values for dates from 1945.0 to 2010.0 inclusive are
+    definitive, otherwise they are non-definitive.
+
+    To get the other geomagnetic elements (D, I, H and secular
+    variations dD, dH, dI and dF) use routines ptoc and ptocsv.
+
+    Adapted from 8th generation version to include new maximum degree for
+    main-field models for 2000.0 and onwards and use WGS84 spheroid instead
+    of International Astronomical Union 1966 spheroid as recommended by IAGA
+    in July 2003. Reference radius remains as 6371.2 km - it is NOT the mean
+    radius (= 6371.0 km) but 6371.2 km is what is used in determining the
+    coefficients. Adaptation by Susan Macmillan, August 2003 (for
+    9th generation), December 2004, December 2009, December 2014.
+
+    Coefficients at 1995.0 incorrectly rounded (rounded up instead of
+    to even) included as these are the coefficients published in Excel
+    spreadsheet July 2005.
+
+    Args:
+        years : year or years A.D. Must be greater than or equal to 1900.0 and
+            less than or equal to 2025.0. Warning message is given
+            for dates greater than 2020.0. Must be double precision.
+        itype : 1 if geodetic (spheroid), 2 if geocentric (sphere)
+        alts : height in km above sea level if itype == 1,
+            distance from centre of Earth in km if itype == 2 (>3485 km)
+        lats : latitude (-90 to 90)
+        elongs : east-longitude (0 to 360)
+        xyzfs : north, east, verical, total; [nT] if isv == 0, [nT/year] if isv == 1 (output)
+    Returns:
+        xyzfs
+    """
+
+    assert years.shape[0] == alts.shape[0]
+    assert years.ndim == 1
+    assert years.dtype == np.float64
+
+    assert itype in (1, 2)
+
+    assert alts.ndim == 1
+    assert alts.dtype == np.float64
+    assert lats.ndim == 1
+    assert lats.dtype == np.float64
+    assert elongs.ndim == 1
+    assert elongs.dtype == np.float64
+    assert alts.shape[0] == lats.shape[0] == elongs.shape[0]
+
+    if xyzfs is None:
+        xyzfs = np.zeros((alts.shape[0], 4), dtype = 'f8')
+    else:
+        assert xyzfs.ndim == 2
+        assert xyzfs.dtype == np.float64
+        assert xyzfs.shape == (alts.shape[0], 4)
+
+    threadsperblock = 32
+    blockspergrid = (xyzfs.shape[0] + (threadsperblock - 1))
+
+    _get_syn[blockspergrid, threadsperblock](
+        years,
+        itype,
+        alts,
+        lats,
+        elongs,
+        GH,
+        xyzfs,
+    )
+
+    return xyzfs
